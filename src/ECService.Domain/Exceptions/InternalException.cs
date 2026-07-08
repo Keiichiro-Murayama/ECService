3b86611e6f3bbb4620c92f68c59d169909b033ba
OgawaYuna03 <yuna.ogawa.howsplan@gmail.com>
2026-07-08 16:34:54 +0900

    InternalException:作成

---
+++ src/ECService.Domain/Exceptions/InternalException.cs
@@ -0,0 +1,38 @@
+namespace ECService.Internal.Exceptions;
+
+/// <summary>
+/// 業務制約を表す例外クラス
+/// </summary>
+public class InternalException : Exception
+{
+    /// <summary>
+    /// 違反した項目名
+    /// エラーレスポンスの組み立てに利用できる
+    /// </summary>
+    public string? ParamName { get; }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public InternalException() : base()
+    {
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="message">エラーメッセージ</param>
+    public InternalException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="message">エラーメッセージ</param>
+    /// <param name="paramName">違反した項目名</param>
+    public InternalException(string message, string paramName) : base(message)
+    {
+        ParamName = paramName;
+    }
+}
