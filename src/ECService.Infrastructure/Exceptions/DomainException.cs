1d73fa909b89acc2b864e50cf976f93b89b0b0ee
OgawaYuna03 <yuna.ogawa.howsplan@gmail.com>
2026-07-09 10:51:41 +0900

    Exception:修正

--- src/ECService.Infrastructure/Exceptions/DomainException.cs
+++ src/ECService.Infrastructure/Exceptions/DomainException.cs
@@ -1,38 +1,10 @@
 namespace ECService.Infrastructure.Exceptions;
-
 /// <summary>
-/// 業務制約を表す例外クラス
+/// 業務ルール違反を表す例外クラス
 /// </summary>
 public class DomainException : Exception
 {
-    /// <summary>
-    /// 違反した項目名
-    /// エラーレスポンスの組み立てに利用できる
-    /// </summary>
-    public string? ParamName { get; }
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    public DomainException() : base()
-    {
-    }
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    /// <param name="message">エラーメッセージ</param>
-    public DomainException(string message, Exception ex) : base(message)
-    {
-    }
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    /// <param name="message">エラーメッセージ</param>
-    /// <param name="paramName">違反した項目名</param>
-    public DomainException(string message, string paramName) : base(message)
-    {
-        ParamName = paramName;
-    }
+    public DomainException(string message) : base(message) { }
+    public DomainException(string message, Exception innerException) 
+    : base(message, innerException) { }
 }
