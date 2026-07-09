1e101a3824ffdef94fda57838a563cf2302e3687
OgawaYuna03 <yuna.ogawa.howsplan@gmail.com>
2026-07-09 11:14:57 +0900

    ProductEntityAdapter:作成

--- src/ECService.Infrastructure/Adapter/ProductEntityAdapter.cs
+++ src/ECService.Infrastructure/Adapter/ProductEntityAdapter.cs
@@ -9,38 +9,25 @@ namespace ECService.Infrastructure.Adapters;
 /// <typeparam name="Product">ドメインオブジェクト:Product</typeparam>
 /// <typeparam name="ProductEntity">EFCore:ProductEntity</typeparam>
 public class ProductEntityAdapter :
-IConverter<Product, ProductEntity>, IRestorer<Product, ProductEntity>
+IConverter<Product, ProductEntity>
 {
     /// <summary>
     /// ドメインオブジェクト:ProductをProductEntityに変換する
     /// </summary>
     /// <param name="domain">ドメインオブジェクト:Product</param>
     /// <returns>EFCore:ProductEntity</returns>
-    public ProductEntity Convert(Product product)
+    public Task<ProductEntity> ConvertAsync(Product domain)
     {
-        return new ProductEntity
-        {
-            ProductUuid = Guid.Parse(product.ProductUuid),
-            Name = product.Name,
-            Price = product.Price,
-            ImageUrl = product.ImageUrl,
-            DeleteFlag = product.DeleteFlg
-            // ProductCategoryId は Repository で設定する
-        };
-    }
 
-    /// <summary>
-    /// ProductEntityからドメインオブジェクト:Productを復元する
-    /// </summary>
-    /// <param name="target">>EFCore:ProductEntity</param>
-    /// <returns>ドメインオブジェクト:Product</returns>
-    public Task<Product> RestoreAsync(ProductEntity target)
-    {
-        // 引数targetがnullの場合
-        _ = target ?? throw new InternalException("引数targetがnullです。");
-        // ProductEntityからドメインオブジェクト:Productを復元する
-        var domain = new Product(target.ProductUuid, target.Name, target.Price, target.ImageUrl, target.DeleteFlag, ProductCategory., ProductStock.productStock);
-        return Task.FromResult(domain);
-    }
+        _ = domain ?? throw new InternalException("引数domainがnullです。");
 
+        var entity = new ProductEntity();
+        entity.ProductUuid = Guid.Parse(domain.ProductUuid);
+        entity.Name = domain.Name;
+        entity.Price = domain.Price;
+        entity.ImageUrl = domain.ImageUrl;
+        entity.DeleteFlag = domain.DeleteFlg;
+        return Task.FromResult(entity);
+
+    }
 }
