12710172981fbd1ca2df80c9e990efec2eef8f99
KatanoKeisuke <keisuke.katano.howsplan@gmail.com>
2026-07-09 10:47:19 +0900

    IGetCategoryUsecase.cs : 作成

---
+++ src/ECService.Application/Usecases/Interfaces/IGetCategoryUsecase.cs
@@ -0,0 +1,13 @@
+using ECService.Domain.Models;
+namespace ECService.Applications.Usecases.Interfaces;
+/// <summary>
+/// 商品登録サービスインターフェイス
+/// </summary>
+public interface IGetCategoryUsecase
+{
+    /// <summary>
+    /// カテゴリ情報を取得する
+    /// </summary>
+    /// <param name="product">永続化する商品</param>
+    Task<List<ProductCategory>> ExecuteAsync();
+}
