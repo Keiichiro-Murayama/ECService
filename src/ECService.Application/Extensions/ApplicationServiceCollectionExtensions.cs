58167192e873da828180c9ffd0e8d38d900a1c6b
Keiichiro-Murayama <Keiichiro.Murayama@wemex.com>
2026-07-10 10:38:20 +0900

    ApplicationServiceCollectionExtensions.cs : create

--- src/ECService.Application/Extensions/ApplicationServiceCollectionExtensions.cs
+++ src/ECService.Application/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -1,117 +1,50 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-//using ECService.Infrastructure.Contexts;
-//using ECService.Domains.Models;
-//using ECService.Infrastructure.Adapters;
-//using ECService.Infrastructure.Repositories;
-//using ECService.Domains.Repositories;
-//using ECService.Infrastructure.Shared;
-//using ECService.Application.Usecases;
-//using ECService.Application.Usecases.Interactors;
-//using ECService.Application.Usecases.Interfaces;
-//using ECService.Presentation.Adapters;
+using ECService.Application;
+using ECService.Application.Authentications;
 
-using ECService.Application.Usecases.Interfaces;
 using ECService.Application.Usecases.Imps;
-
-using ECService.Application.Security;
-using Microsoft.AspNetCore.Identity;
-///using Microsoft.IdentityModel.Tokens;
-using ECService.Application.Employees.Interactors;
-using ECService.Application.Usecases.Employees.Interfaces;
-///using ECService.Infrastructure.Security;
-///using ECService.Application.Usecases.Authenticate.Interactors;
-///using ECService.Application.Usecases.Authenticate.Interfaces;
+using ECService.Application.Usecases.Interfaces;
+using ECService.Domain.Adapters;
 using ECService.Domain.Models;
-namespace ECService.Presentation.Configs;
+using Microsoft.Extensions.DependencyInjection;
+namespace ECService.Application.Extensions;
 /// <summary>
-/// 依存関係(DI)の設定
-/// インフラストラクチャ層、アプリケーション層、プレゼンテーション層
-/// をまとめて追加する拡張クラス
+/// アプリケーション層の構成要素を DI コンテナへ登録する拡張メソッドを提供する
 /// </summary>
 public static class ApplicationServiceCollectionExtensions
 {
     /// <summary>
-    /// アプリ全体の依存関係を一括追加する拡張メソッド
+    /// アプリケーション層の構成要素(Service・UseCase・認証コンポーネントなど)を登録する
     /// </summary>
-    /// <param name="services">サービスコレクション</param>
-    /// <param name="config">構成情報</param>
-    /// <returns>IServiceCollection(チェーン可能)</returns>
-    public static IServiceCollection AddInfrastructure(
-        this IServiceCollection services, string connectionString)
+    /// <param name="services">DI コンテナ</param>
+    /// <param name="jwtSettings">JWT の設定値(プレゼンテーション層が外部設定から組み立てて渡す)</param>
+    /// <returns>登録後の DI コンテナ(メソッドチェーン用)</returns>
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        JwtSettings jwtSettings)
     {
-        // インフラストラクチャ層の依存関係を追加
-        services.AddInfrastructureDependencies(config);
-        // アプリケーション層の依存関係を追加
-        services.AddApplicationLayerDependencies(config);
-        // プレゼンテーション層の依存関係を追加
-        services.AddPresentationLayerDependencies(config);
-        return services;
-    }
-
-    /// <summary>
-    /// アプリケーション層の依存関係を追加
-    /// </summary>
-    /// <param name="services">依存関係注入(DI)のサービスコレクション</param>
-    /// <param name="config"></param>
-    /// <returns></returns>
-    private static IServiceCollection AddApplicationLayerDependencies(
-        this IServiceCollection services, IConfiguration config)
-    {
-        //ユースケース：[新商品を登録する]を実現するインターフェース
-        //services.AddScoped<IRegisterBookUsecase, RegisterBookUsecase>();
-        //ユースケース[商品を変更する]を実現するインターフェース
-        //services.AddScoped<IUpdateBookUsecase, UpdateBookUsecase>();
-        //ユースケース[商品をキーワード検索する]を実現するインターフェース
-        //services.AddScoped<ISearchBookByKeywordUsecase, SearchBookByKeywordUsecase>();
-        //ユースケース：[新商品を削除する]を実現するインターフェース
-        // services.AddScoped<IDeleteBookUsecase, DeleteBookUsecase>();
-        // UpdateProductViewModelからドメインオブジェクト:Productへ変換するアダプタ
-        // ASP.NET Core Identityのパスワードハッシュ化・検証機能
-        services.AddScoped<IPasswordHasher<EmployeeAccount>, PasswordHasher<EmployeeAccount>>();
-        // PBKDF2アルゴリズムを利用したパスワードハッシュ化・検証機能
-        services.AddScoped<IPasswordService, PasswordService>();
-        // ユースケース:[ユーザーを登録する]を実現するインターフェイス
-        services.AddScoped<IRegisterEmployeeAccountUsecase, RegisterEmployeeAccountUsecase>();
-        // JwtSettingsをバインドしてDIに登録する
-        //services.Configure<JwtSettings>(config.GetSection("JwtSettings"));
-        // ユースケース:[ログインする]を実現するインターフェイス
-        // services.AddScoped<IAuthenticateUserUsecase, AuthenticateUserUsecase>();
 
 
+        // UseCase(Service・Adapter に依存するため Scoped)
+        // services.AddScoped<IFindCategoriesUseCase, FindCategoriesInteractor>();
+        // services.AddScoped<ISearchBooksUseCase, SearchBooksInteractor>();
+        // services.AddScoped<IFindBookUseCase, FindBookInteractor>();
+        // services.AddScoped<IRegisterBookUseCase, RegisterBookInteractor>();
+        // services.AddScoped<IUpdateBookUseCase, UpdateBookInteractor>();
+        // services.AddScoped<IDeleteBookUseCase, DeleteBookInteractor>(); // 追加(抜けていた)
 
+        // パスワードのハッシュ化・照合を行うコンポーネント(状態を持たないため Singleton)
+        // services.AddSingleton<IPasswordService, PasswordService>();
 
+        // JWT の設定値を登録し、JwtTokenProvider に注入できるようにする
+        // services.AddSingleton(jwtSettings);
+        // JWT の発行を行うコンポーネント(状態を持たないため Singleton)
+        // services.AddSingleton<IJwtTokenProvider, JwtTokenProvider>();
 
-        // ユースケース:[商品を検索する]を実現するインターフェイス
-        services.AddScoped<ISearchProductsUseCase, SearchProductsUseCase>();
+        // services.AddScoped<IRegisterUserUseCase, RegisterUserInteractor>();
+        // services.AddSingleton<IAdapter<User, UserDto>, UserDtoAdapter>();
 
+        // services.AddScoped<ILoginUseCase, LoginInteractor>();
 
         return services;
     }
 }
-
-
-/// <summary>
-/// テストプロジェクトにServiceProviderを提供するヘルパメソッド
-/// </summary>
-/// <param name="config"></param>
-/// <param name="configureServices"></param>
-/// <param name="configureLogging"></param>
-/// <returns></returns>
-/* public static ServiceProvider BuildAppProvider(
-    IConfiguration config,
-    Action<IServiceCollection>? configureServices = null,
-    Action<ILoggingBuilder>? configureLogging = null)
- {
-     var services = new ServiceCollection();
-     services.AddLogging(b =>
-     {
-         if (configureLogging is not null) configureLogging(b);
-         else b.AddConsole().SetMinimumLevel(LogLevel.Warning);
-     });
-     services.AddApplicationDependencies(config);
-     configureServices?.Invoke(services);
-
-     return services.BuildServiceProvider(validateScopes: true);
- }
-}*/
