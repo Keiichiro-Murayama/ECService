51724fa7a7782008e9365f5bfc53e5f200a999c9
Keiichiro-Murayama <Keiichiro.Murayama@wemex.com>
2026-07-14 14:55:14 +0900

    Migrations/*.sql : fix and create

--- src/ECService.Infrastructure/Migrations/20260714044207_AddForCustomerAPI.Designer.cs
+++ src/ECService.Infrastructure/Migrations/20260714044207_AddForCustomerAPI.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ECService.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ECService.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260714044207_AddForCustomerAPI")]
+    partial class AddForCustomerAPI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
